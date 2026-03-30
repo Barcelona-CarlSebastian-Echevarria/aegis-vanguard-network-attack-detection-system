a9fd9ba8d0fe1306d8754e266def53aeda83c34e
Carl from Linux <carlsebastian.e.barcelona@gmail.com>
2026-03-30 15:30:23 +0800

    Uploaded packet capture mechanism and CICFlowmeter. CICFlowmeter will be credited accordingly as this is not mine

--- packet_capture.py
+++ packet_capture.py
@@ -1,9 +1,12 @@
 import subprocess
 import os
 from pathlib import Path
+from datetime import date
+
+ROOT_DIR = Path.cwd()
+pcap_path = os.path.join(ROOT_DIR, "packets_captured", "traffic1.pcap")
 
 def capture(interface="Wi-Fi"):
-    ROOT_DIR = Path.cwd()
     target_dir = os.path.join(ROOT_DIR, "packets_captured")
     
     # Create the directory if it doesn't exist
@@ -20,4 +23,29 @@ def capture(interface="Wi-Fi"):
         "-w", output_file
     ])
 
-capture()
+def convert_packets(pcap_path, output_name: str):
+    if not isinstance(output_name, str):
+        raise ValueError("outname not string") 
+
+    csv_directory = os.path.join(ROOT_DIR, "converted_flows")
+
+    if not os.path.exists(csv_directory):
+        os.makedirs(csv_directory)
+
+    output_path = os.path.join(csv_directory, output_name)
+    
+    subprocess.run([
+        "cicflowmeter",
+        "-f", str(pcap_path),
+        "-c", f"{output_path}.csv"
+    ], check=True)
+
+    return f"{output_path}.csv"  
+
+
+if __name__ == '__main__':
+    # capture()
+    csv_file = convert_packets(pcap_path, 'flow_output')
+    if not os.path.exists(csv_file):
+        print(csv_file, "not found in directory")
+    print(csv_file, "successfully create")
